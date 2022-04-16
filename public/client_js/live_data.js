58c56dd6a2db08dc4011c9fa69152abb248f0e8e
devibadt <devibadt@coventry.ac.uk>
2022-04-16 02:15:12 +0100

    added a table which shows realtime data on the admin page

--- public/client_js/live_data.js
+++ public/client_js/live_data.js
@@ -19,15 +19,89 @@ function connect(host) {
 
     }
     
+    
     ws.onmessage = function (evt) {
+            // let table = document.getElementById("bookingTable")
+            // table.oldHTML = table.innerHTML
 
+            //const a = document.getElementById("header")
+            //a.innerText = "TUSHAR"
             
+
             let data = evt.data
-            const arr = evt.data
+           // const arr = evt.data
             
             try{
-                console.log(JSON.parse(data))
+                const bookings = JSON.parse(data)
+                //console.log(bookings[0])
+              //  const all = document.querySelectorAll('td')
+               
+              
+               console.clear()
+ 
+           
+                let table = document.getElementById("bookingTable")
+               
+               
+            //    var child = template.lastElementChild; 
+            //     while (child) {
+            //         template.removeChild(child);
+            //         child = template.lastElementChild;
+            //     }
 
+               let div = document.getElementById("records")
+
+           
+                div.innerHTML = ""
+              
+               for(const item of bookings){
+                
+                  // let node = body.cloneNode(true)
+                   //NEW object (booking)
+                   console.log(item.carReg)
+                   console.log(item.username)
+                   console.log(item.start_dateTime)
+                   console.log(item.end_dateTime)
+                   console.log("********************")
+                
+
+                   //driver name
+                   let name  = document.createElement("td")
+                   name.innerText  = item.username
+
+                   //car registation plate
+                   let carReg = document.createElement("td")
+                   carReg.innerText = item.carReg
+
+                   // start date time
+                   let s_date = document.createElement("td")
+                   s_date.innerText = item.start_dateTime
+
+                // end date time
+                   let e_date = document.createElement("td")
+                   e_date.innerText = item.start_dateTime
+
+
+
+                   let tr  = document.createElement('tr')
+
+                   
+
+                    tr.appendChild(name)
+                    tr.appendChild(carReg)
+                    tr.appendChild(s_date)
+                    tr.appendChild(e_date)
+                  
+
+                   div.appendChild(tr)
+
+                   table.appendChild(div)
+                   
+                   
+
+               }
+    
+                
             }catch(err){
                 console.log("cannot get live bookings")
                 console.log(err.message)
